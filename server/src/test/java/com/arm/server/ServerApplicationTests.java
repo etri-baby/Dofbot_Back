d9e0c412306b5bb2df037fff9bb438113fa655bf
현수 <znight1020@naver.com>
2023-07-19 13:48:15 +0900

    init

---
+++ server/src/test/java/com/arm/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
